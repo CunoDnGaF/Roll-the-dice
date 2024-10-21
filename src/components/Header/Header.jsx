f46d8d0a581b9aec3a454c738c94baa6ab3d0c5b
Nimat13 <nikitamatrosov4@gmail.com>
2024-10-21 12:59:44 +0300

    mobile adoption

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,8 +3,10 @@ import Complexity from './Complexity/Complexity';
 function Header() {
   return (
     <header>
-      <img className="header-logo" src="./src/assets/d20_logo.png"></img>
-      <h1 className="header-title">Roll <br></br> The Dice</h1>
+      <div className="header-logo-container">
+        <img className="header-logo" src="./src/assets/d20_logo.png"></img>
+        <h1 className="header-title">Roll <br></br> The Dice</h1>
+      </div>
       <Complexity />
     </header>
   )
